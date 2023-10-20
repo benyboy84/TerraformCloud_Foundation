5ae9f7cc4b4733323fb24dea12166787264f02c8
BenyBoy84 <Blais_Benoit@hotmail.com>
2023-10-20 10:48:08 -0400

    ft: Workspace

--- locals.tf
+++ locals.tf
@@ -3,7 +3,7 @@ locals {
   organization_name = "ConseilsTI"
 
   # This local is used to define Oauth_client name.
-  # oauth_client_name = "GitHub.com"
+  oauth_client_name = "GitHub.com"
 
   # This local is used to define teams at the organization level.
   organization_teams = {
