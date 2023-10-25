90c9ab695dbea46ce0b7fc0fb4c7cb75174244da
GitHub Action <notifications@github.com>
2023-10-25 20:15:07 +0000

    Automated Terraform Formatting

--- resources_variable.tf
+++ resources_variable.tf
@@ -15,12 +15,12 @@ resource "tfe_variable" "variable_set" {
 resource "tfe_variable" "workspace" {
   for_each = { for variable in local.workspace_variables : "${variable.workspace} ${variable.key}" => variable }
 
-  key             = each.value.key
-  value           = each.value.value
-  category        = each.value.category
-  description     = try(each.value.description, null)
-  hcl             = try(each.value.hcl, null)
-  sensitive       = try(each.value.sensitive, null)
-  workspace_id    = module.workspaces[each.value.workspace].id
+  key          = each.value.key
+  value        = each.value.value
+  category     = each.value.category
+  description  = try(each.value.description, null)
+  hcl          = try(each.value.hcl, null)
+  sensitive    = try(each.value.sensitive, null)
+  workspace_id = module.workspaces[each.value.workspace].id
 
 }
