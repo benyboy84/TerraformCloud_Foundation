b03854c282d7d1617af438480f960a2759cc9b6c
BenyBoy84 <Blais_Benoit@hotmail.com>
2023-10-25 16:13:24 -0400

    ft: Variables

---
+++ locals_variable.tf
@@ -0,0 +1,34 @@
+locals {
+
+  # The following locals use logic to determine the variable associated to a variable sets.
+  variable_sets_variables = flatten([for variable_set in local.variable_sets :
+    flatten([for key, variable in variable_set.variables :
+      merge(
+        variable,
+        {
+          name         = key
+          variable_set = variable_set.name
+        }
+      )
+    ])
+    if try(variable_set.variables,null) != null
+  ])
+
+  # The following locals use logic to determine the variable associated to a workspace.
+  workspace_variables = flatten([for project_key, project in local.projects :
+    flatten([for workspace_key, workspace in project.workspaces :
+      flatten([for variable_key, variable in workspace.variables :
+        merge(
+          variable,
+          {
+            name      = variable_key
+            workspace = workspace_key
+          }
+        )
+      ])
+      if try(workspace.variables, null) != null
+    ])
+    if try(project.workspaces, null) != null
+  ])
+
+}
---
+++ resources_variable.tf
@@ -0,0 +1,26 @@
+# The following code block is used to create variable resources.
+resource "tfe_variable" "variable_set" {
+  for_each = { for variable in local.variable_sets_variables : "${variable.variable_set} ${variable.key}" => variable }
+
+  key             = each.value.key
+  value           = each.value.value
+  category        = each.value.category
+  description     = try(each.value.description, null)
+  hcl             = try(each.value.hcl, null)
+  sensitive       = try(each.value.sensitive, null)
+  variable_set_id = tfe_variable_set.this[each.value.variable_set].id
+
+}
+
+resource "tfe_variable" "workspace" {
+  for_each = { for variable in local.workspace_variables : "${variable.workspace} ${variable.key}" => variable }
+
+  key             = each.value.key
+  value           = each.value.value
+  category        = each.value.category
+  description     = try(each.value.description, null)
+  hcl             = try(each.value.hcl, null)
+  sensitive       = try(each.value.sensitive, null)
+  workspace_id    = module.workspaces[each.value.workspace].id
+
+}
