b03f66c4475090b461139406fe3185f801cb210f
BenyBoy84 <Blais_Benoit@hotmail.com>
2023-10-25 11:21:15 -0400

    ft: Variable sets

--- resources_variable_set.tf
+++ resources_variable_set.tf
@@ -19,13 +19,13 @@ resource "tfe_variable_set" "this" {
 resource "tfe_project_variable_set" "this" {
   for_each = { for object in local.organization_variable_sets_project : "${object.name} ${object.project}" => object }
 
-  variable_set_id = tfe_variable_set.this["${each.value.name}"].id
-  project_id      = tfe_project.project["${each.value.project}"].id
+  variable_set_id = tfe_variable_set.this[each.value.name].id
+  project_id      = tfe_project.project[each.value.project].id
 }
 
 resource "tfe_workspace_variable_set" "this" {
   for_each = { for object in local.organization_variable_sets_workspace : "${object.name} ${object.workspace}" => object }
 
-  variable_set_id = tfe_variable_set.this["${each.value.name}"].id
-  workspace_id    = try(module.workspaces["${each.value.workspace}"].id, null)
+  variable_set_id = tfe_variable_set.this[each.value.name].id
+  workspace_id    = try(module.workspaces[each.value.workspace].id, null)
 }
