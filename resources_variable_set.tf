25a8dc4ec894646de155cb7d473a5db48211279b
BenyBoy84 <Blais_Benoit@hotmail.com>
2023-10-25 08:58:23 -0400

    ft: Variable sets

--- resources_variable_set.tf
+++ resources_variable_set.tf
@@ -1,23 +1,39 @@
 # The following code block is used to create variable sets resources.
 
-module "variable_set" {
-  source = "./modules/variable_set"
-
-  for_each = local.variable_sets
+resource "tfe_variable_set" "this" {
+for_each = local.variable_sets
 
   name         = each.key
-  description  = try(each.value.description, null)
-  global       = try(each.value.global, null)
+  description  = try(each.value.description,null)
+  global       = try(each.value.global,null)
   organization = data.tfe_organization.this.name
-  workspaces = try(
-    flatten([for workspace in flatten(each.value.workspaces) : {
-      name = workspace
-      id   = try(module.workspaces["${workspace}"].id, null)
-  }]), [])
-  projects = try(
-    flatten([for project in flatten(each.value.projects) : {
-      name = project
-      id   = try(module.workspaces["${project}"].id, null)
-  }]), [])
 
+  lifecycle {
+    precondition {
+      condition     = var.global ? var.workspaces == null && var.projects == null ? true : false : true
+      error_message = "`global` cannot be set to true if `workspaces` or `projects` is defined."
+    }
+  }
 }
+
+# module "variable_set" {
+#   source = "./modules/variable_set"
+
+#   for_each = local.variable_sets
+
+#   name         = each.key
+#   description  = try(each.value.description, null)
+#   global       = try(each.value.global, null)
+#   organization = data.tfe_organization.this.name
+#   workspaces = try(
+#     flatten([for workspace in flatten(each.value.workspaces) : {
+#       name = workspace
+#       id   = try(module.workspaces["${workspace}"].id, null)
+#   }]), null)
+#   projects = try(
+#     [for project in flatten(each.value.projects) : {
+#       name = project
+#       id   = try(module.workspaces["${project}"].id, null)
+#   }], null)
+
+# }
