f39722ad12584a6866a24a714a372ce79cd6a76c
BenyBoy84 <Blais_Benoit@hotmail.com>
2023-10-26 10:02:01 -0400

    ft: Variable

--- locals_variable.tf
+++ locals_variable.tf
@@ -1,6 +1,6 @@
 locals {
 
-  # The following locals use logic to determine the variable associated to a variable sets excluding any TFE_TOKEN variable.
+  # The following locals use logic to determine the variable associated to a variable sets.
   variable_sets_variables = flatten([for variable_set in local.variable_sets :
     flatten([for variable_key, variable in variable_set.variables :
       merge(
@@ -9,26 +9,11 @@ locals {
           key          = variable_key
           variable_set = variable_set.name
         }
-      ) if variable_key != "TFE_TOKEN"
+      )
     ])
     if try(variable_set.variables, null) != null
   ])
-
-  # The following locals use logic to determine TFE_TOKEN variable associated to a variable sets.
-  variable_sets_variables_tfe_token = flatten([for variable_set in local.variable_sets :
-    flatten([for variable_key, variable in variable_set.variables :
-      merge(
-        variable,
-        {
-          key          = variable_key
-          variable_set = variable_set.name
-        }
-      ) if variable_key == "TFE_TOKEN"
-    ])
-    if try(variable_set.variables, null) != null
-  ])
-
-  # The following locals use logic to determine the variable associated to a workspace excluding any TFE_TOKEN variable.
+  # The following locals use logic to determine the variable associated to a workspace.
   workspace_variables = flatten([for project_key, project in local.projects :
     flatten([for workspace_key, workspace in project.workspaces :
       flatten([for variable_key, variable in workspace.variables :
@@ -38,24 +23,7 @@ locals {
             key       = variable_key
             workspace = workspace_key
           }
-        ) if variable_key != "TFE_TOKEN"
-      ])
-      if try(workspace.variables, null) != null
-    ])
-    if try(project.workspaces, null) != null
-  ])
-
-  # The following locals use logic to determine TFE_TOKEN variable associated to a workspace.
-  workspace_variables_tfe_token = flatten([for project_key, project in local.projects :
-    flatten([for workspace_key, workspace in project.workspaces :
-      flatten([for variable_key, variable in workspace.variables :
-        merge(
-          variable,
-          {
-            key       = variable_key
-            workspace = workspace_key
-          }
-        ) if variable_key == "TFE_TOKEN"
+        )
       ])
       if try(workspace.variables, null) != null
     ])
--- resources_variable.tf
+++ resources_variable.tf
@@ -1,7 +1,7 @@
 # The following code block is used to create variable resources.
 
 resource "tfe_variable" "variable_set" {
-  for_each = { for variable in local.variable_sets_variables : "${variable.variable_set} ${variable.key}" => variable }
+  for_each = { for variable in local.variable_sets_variables : "${variable.variable_set} ${variable.key}" => variable if variable.key != "TFE_TOKEN" }
 
   key             = each.value.key
   value           = each.value.value
@@ -13,7 +13,7 @@ resource "tfe_variable" "variable_set" {
 }
 
 resource "tfe_variable" "variable_set_tfe_token" {
-  for_each = { for variable in local.variable_sets_variables_tfe_token : "${variable.workspace} ${variable.key}" => variable }
+  for_each = { for variable in local.variable_sets_variables_tfe_token : "${variable.workspace} ${variable.key}" => variable if variable.key == "TFE_TOKEN" }
 
   key             = each.value.key
   value           = try(module.teams[each.value.value].token_id, each.value.value)
