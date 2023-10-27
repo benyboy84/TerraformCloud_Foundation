8b3602c078fa4a95c627ff5a856fe9a269a5a357
BenyBoy84 <Blais_Benoit@hotmail.com>
2023-10-26 20:23:12 -0400

    ft: Workspace

--- locals_workspace.tf
+++ locals_workspace.tf
@@ -1,7 +1,7 @@
 locals {
 
   # The following locals use logic to determine the project associate with each workspace.
-  workspaces_array = flatten([for projects, project in local.projects :
+  workspaces = flatten([for projects, project in local.projects :
     flatten([for workspaces, workspace in project.workspaces :
       merge(
         workspace,
@@ -11,6 +11,5 @@ locals {
     ])
     if try(project.workspaces, null) != null
   ])
-  workspaces = { for row in local.workspaces_array : row.name => row }
 
 }
