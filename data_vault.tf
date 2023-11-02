6fb4f0127ebbc3bfad0b87533d2bfd27f9937631
BenyBoy84 <Blais_Benoit@hotmail.com>
2023-11-02 15:57:26 -0400

    ft: AWS_OIDC_TerraformCloud workspace

--- data_vault.tf
+++ data_vault.tf
@@ -1,10 +1,10 @@
 # The following block are used to retrieve secrets and their latest version values for a given application.
-data "hcp_vault_secrets_secret" "AWS_Access_Key_ID" {
+data "hcp_vault_secrets_secret" "Aws_Access_Key_Id" {
   app_name    = "AWS"
   secret_name = "AWS_ACCESS_KEY_ID"
 }
 
-data "hcp_vault_secrets_secret" "AWS_Secret_Access_Key" {
+data "hcp_vault_secrets_secret" "Aws_Secret_Access_Key" {
   app_name    = "AWS"
   secret_name = "AWS_ACCESS_KEY_ID"
 }
