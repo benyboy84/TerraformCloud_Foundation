c3b864338a96065d1f9023936f73884b6fba8c28
BenyBoy84 <Blais_Benoit@hotmail.com>
2023-11-02 15:37:10 -0400

    ft: Organization Team

--- locals_team.tf
+++ locals_team.tf
@@ -11,7 +11,7 @@ locals {
   ])
 
   # The following locals use logic to determine the project associate with each team.
-  project_level_teams = flatten([for project_key, project in local.projects :
+  project_level_teams = nonsensitive(flatten([for project_key, project in local.projects :
     flatten([for team_key, team in project.teams :
       merge(
         team,
@@ -21,10 +21,10 @@ locals {
       )
     ])
     if try(project.teams, null) != null
-  ])
+  ]))
 
   # The following locals use logic to determine the workspace associate with each team.
-  workspace_level_teams = flatten([for project_key, project in local.projects :
+  workspace_level_teams = nonsensitive(flatten([for project_key, project in local.projects :
     flatten([for workspace_key, workspace in project.workspaces :
       flatten([for team_key, team in workspace.teams :
         merge(
@@ -37,7 +37,7 @@ locals {
       if try(workspace.teams, null) != null
     ])
     if try(project.workspaces, null) != null
-  ])
+  ]))
 
   # This is to concat organization teams with project teams.
   teams = concat(
