1c4c01121325f64a077abff1b4c1ace3bc69f834
GitHub Action <notifications@github.com>
2023-11-03 12:13:29 +0000

    Automated Terraform Formatting

--- data_vault.tf
+++ data_vault.tf
@@ -1,7 +1,7 @@
 # The following block are used to retrieve secrets and their latest version values for a given application.
 data "hcp_vault_secrets_secret" "this" {
   for_each = local.secrets
-  
+
   app_name    = each.value.project
   secret_name = each.key
 }
