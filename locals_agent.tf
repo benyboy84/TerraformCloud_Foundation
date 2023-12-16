3aa03735b27da59ecd5fd0fca736eb3a26e9bcd5
GitHub Action <notifications@github.com>
2023-12-16 14:07:03 +0000

    Automated Terraform Formatting

--- locals_agent.tf
+++ locals_agent.tf
@@ -3,7 +3,7 @@ locals {
   # The following locals use logic to determine the required agent pool.
   agent_pools = flatten([for project_key, project in local.projects :
     flatten([for agent_pool_key, agent_pool in toset(project.agent_pools) :
-        lower(agent_pool_key)
+      lower(agent_pool_key)
     ])
     if try(project.agent_pools, null) != null
   ])
