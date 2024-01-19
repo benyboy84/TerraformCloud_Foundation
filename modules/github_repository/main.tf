2efccdd8082d5d91bfc72b5e1cb1105cfcecafbd
BenyBoy84 <Blais_Benoit@hotmail.com>
2024-01-19 09:22:26 -0500

    ft: GitHub

--- modules/github_repository/main.tf
+++ modules/github_repository/main.tf
@@ -147,7 +147,7 @@ resource "github_branch_protection_v3" "this" {
 }
 
 resource "github_actions_secret" "this" {
-  for_each        = { for secret in var.secrets : "${secret.secret_name}" => secret }
+  for_each        = { for secret in var.secrets : secret.secret_name => secret }
   repository      = github_repository.this.name
   secret_name     = each.value.secret_name
   plaintext_value = each.value.plaintext_value
