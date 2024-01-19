b876872db874ebae15d18258587e733dbd42bd52
BenyBoy84 <Blais_Benoit@hotmail.com>
2024-01-19 10:08:35 -0500

    ft: GitHub

--- modules/github_repository/main.tf
+++ modules/github_repository/main.tf
@@ -111,7 +111,6 @@ resource "github_branch_protection" "this" {
 
 }
 
-
 # resource "github_branch_protection_v3" "this" {
 #   repository                      = github_repository.this.name
 #   branch                          = var.branch
